c9295ed5ee2e86f55326a9dcb09aef0ccb826ff0
Jason Isler <jasonisler@jasons-mac-mini.local>
2020-03-21 10:46:04 -0400

    Initial Commit

---
+++ Randog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Randog
+//
+//  Created by Jason Isler on 3/21/20.
+//  Copyright © 2020 Jason Isler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
